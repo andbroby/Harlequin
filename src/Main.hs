1a705f35c5b1c33e421a26af104503f02cb98f77
andbroby <and.broby@gmail.com>
2015-04-17 13:35:34 +0200

    add REPL

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import System.Environment
+import System.IO
 import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Numeric
@@ -12,10 +13,7 @@ import Data.Ratio
 import qualified Data.Array as A
 import Control.Monad.Error
 
-main = do
-  args <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+
 
 data LispError = NumArgs Integer [LispVal]
                | TypeMismatch String LispVal
@@ -70,13 +68,42 @@ instance Error LispError where
 
 type ThrowsError = Either LispError
 
+main = do
+  args <- getArgs
+  case length args of
+   0 -> runRepl
+   1 -> evalAndPrint $ head args
+   otherwise -> putStrLn "Program takes only 0 or 1 argument"
+   
 trapError action = catchError action (return . show)
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+     then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Harlequin>>> ") evalAndPrint
 
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
     
 unwordList :: [LispVal] -> String
 unwordList = unwords . map show
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
